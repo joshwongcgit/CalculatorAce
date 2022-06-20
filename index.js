f5b154b4c7ee25e74bd7c61c7a0833899d9554a0
Joshua Wongq <powerlightlabs@gmail.com>
2022-06-20 16:36:56 -0700

    final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require("fs");
 
 http.createServer(function(req, res) {
- fs.readFile("index.js","utf8" ,function(err, contents){
+ fs.readFile("project.html","utf8" ,function(err, contents){
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write(contents);
  res.end();
