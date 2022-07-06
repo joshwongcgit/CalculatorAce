f3204b4d080ae7d9a8477e02eeea8f0834147fce
Joshua Wongq <powerlightlabs@gmail.com>
2022-07-06 12:48:40 -0700

    final

--- Result/caret.js
+++ Result/caret.js
@@ -7,6 +7,18 @@ function caretPositionLocations(cont)
   return caretPositionsArr;
 } 
 
+ function hideKeyboard(element) {
+    element.attr('readonly', 'readonly'); // Force keyboard to hide on input field.
+    element.attr('disabled', 'true'); // Force keyboard to hide on textarea field.
+    setTimeout(function() {
+        element.blur();  //actually close the keyboard
+        // Remove readonly attribute after keyboard is hidden.
+        element.removeAttr('readonly');
+        element.removeAttr('disabled');
+    }, 100);
+}
+
+
 function clickCaret(previousPos,caretArr)
 {
   $(".sepEqs").off().click(function()
