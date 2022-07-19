212f3fb31bba0059556fd21a2ff0fe4a8ad3bfe6
Joshua Wongq <powerlightlabs@gmail.com>
2022-07-18 17:13:15 -0700

    final

--- Answers/answerIsPressed.js
+++
@@ -1,8 +0,0 @@
-function answerIsPressed(answer)
-{
-  $currentCalc.append('<b class = "number">' + answer + '</b>');
-  sign = false;
-  numbPressed = false;
-  renderAnswers();
-  numberBeforeEqual();
-}
