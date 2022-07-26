51d93154450b9f9896d944974b15a0cb9b1a6585
Joshua Wongq <powerlightlabs@gmail.com>
2022-07-26 07:54:53 -0700

    final:

--- Tab/react.js
+++ Tab/react.js
@@ -6,9 +6,24 @@ class Square extends React.Component
   	(
   	  <button class = "Square">	
   	)
+
+  	 constructor(props) 
+  	 {
+      super(props);
+    this.state = 
+    {
+      value: null,
+    };
   }
 }
 
 
 const root = ReactDOM.createRoot(document.getElementByID("root").render("<Game />");
-	
+
+
+function ()
+{
+	console.log("click");
+}
+
+() => console.log("click")
